535d53295390122371fd0b0bfbd659bbc7402403
Thiago-Teofilo <thiago.teophilos@gmail.com>
2023-05-05 14:32:46 -0300

    Aplicando aula 372 e 373 na calculadora

--- python_curso_completo/m07_ui_pyside6/aula347_calculadora/buttons.py
+++ python_curso_completo/m07_ui_pyside6/aula347_calculadora/buttons.py
@@ -2,7 +2,7 @@ from PySide6.QtWidgets import QPushButton, QGridLayout
 from PySide6.QtCore import Slot
 
 from variables import MEDIUM_FONT_SIZE
-from utils import isEmpty, isNumOrDot, isValidNumber
+from utils import isEmpty, isNumOrDot, isValidNumber, convertToNumber
 
 from typing import TYPE_CHECKING
 import math
@@ -32,7 +32,7 @@ class ButtonsGrid(QGridLayout):
             ['7', '8', '9', '*'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
-            ['',  '0', '.', '='],
+            ['N', '0', '.', '='],
         ]
         self.display = display
         self.info = info
@@ -55,15 +55,12 @@ class ButtonsGrid(QGridLayout):
         self._equation = value
         self.info.setText(value)
 
-    def vouApagarVocê(self, *args):
-        print('Signal recebido em', type(self).__name__, args)
-
     def _makeGrid(self):
-        self.display.eqPressed.connect(self.vouApagarVocê)
-        self.display.delPressed.connect(self.display.backspace)
-        self.display.clearPressed.connect(self.vouApagarVocê)
-        self.display.inputPressed.connect(self.vouApagarVocê)
-        self.display.operatorPressed.connect(self.vouApagarVocê)
+        self.display.eqPressed.connect(self._eq)
+        self.display.delPressed.connect(self._backspace)
+        self.display.clearPressed.connect(self._clear)
+        self.display.inputPressed.connect(self._insertToDisplay)
+        self.display.operatorPressed.connect(self._configLeftOp)
 
         for i, row in enumerate(self._gridMask):
             for j, buttonText in enumerate(row):
@@ -74,7 +71,7 @@ class ButtonsGrid(QGridLayout):
                     self._configSpecialButton(button)
 
                 self.addWidget(button, i, j)
-                buttonSlot = self._makeSlot(self._insertButtonTextToDisplay, button)
+                buttonSlot = self._makeSlot(self._insertToDisplay, buttonText)
                 self._connectButtonClicked(button, buttonSlot)
 
     def _connectButtonClicked(self, button, slot):
@@ -87,41 +84,58 @@ class ButtonsGrid(QGridLayout):
             self._connectButtonClicked(button, self._clear)
 
         if text == '◀':
-            self._connectButtonClicked(button, self.display.backspace)
+            self._connectButtonClicked(button, self._backspace)
+
+        if text == 'N':
+            self._connectButtonClicked(button, self._invertNumber)
 
         if text in '+-/*^':
             self._connectButtonClicked(
                 button, 
-                self._makeSlot(self._operatorClicked, button)
+                self._makeSlot(self._configLeftOp, button.text())
                 )
 
         if text == '=':
             self._connectButtonClicked(button, self._eq)
 
-
+    @Slot()
     def _makeSlot(self, func, *args, **kwargs):
         @Slot(bool)
         def realSlot(_):
             func(*args, **kwargs)
         return realSlot
+    
+    @Slot()
+    def _invertNumber(self):
+        displayText = self.display.text()
 
-    def _insertButtonTextToDisplay(self, button):
-        buttonText = button.text()
-        newDisplayValue = self.display.text() + buttonText
+        if not isValidNumber(displayText):
+            return
+        
+        number = convertToNumber(displayText) * -1
+
+        self.display.setText(str(number))
+        
+    @Slot()
+    def _insertToDisplay(self, text):
+        newDisplayValue = self.display.text() + text
 
         if not isValidNumber(newDisplayValue):
             return
-        self.display.insert(buttonText)
+        self.display.insert(text)
+        self.display.setFocus()
 
+    @Slot()
     def _clear(self):
         self._left = None
         self._right = None
         self._op = None
         self.equation = self._equationInitialValue
         self.display.clear()
+        self.display.setFocus()
 
-    def _operatorClicked(self, button):
-        buttonText = button.text() # +-/* 
+    @Slot()
+    def _configLeftOp(self, text):
         displayText = self.display.text() # Deverá ser o número _left
         self.display.clear()
 
@@ -134,25 +148,28 @@ class ButtonsGrid(QGridLayout):
         # Se não houver algo no número da esquerda,
         # não faremos nada. Aguardaremos o número da direita
         if self._left is None:
-            self._left = float(displayText)
+            self._left = convertToNumber(displayText)
 
-        self._op = buttonText
+        self._op = text
         self.equation = f'{self._left} {self._op} ??'
+        self.display.setFocus()
 
+    @Slot()
     def _eq(self):
         displayText = self.display.text()
 
-        if not isValidNumber(displayText):
+        if not isValidNumber(displayText) or self._left is None:
             self._showError('Conta Incompleta.')
             return
         
-        self._right = float(displayText)
+        self._right = convertToNumber(displayText)
         self.equation = f'{self._left} {self._op} {self._right}'
         result = 'error'
 
         try:
-            if '^' in self.equation and isinstance(self._left, float):
+            if '^' in self.equation and isinstance(self._left, int | float):
                 result = math.pow(self._left, self._right)
+                result = convertToNumber(str(result))
             else:
                 result = eval(self.equation)
         except ZeroDivisionError:
@@ -163,10 +180,16 @@ class ButtonsGrid(QGridLayout):
         self.display.clear()
         self.info.setText(f'{self.equation} = {result}')
         self._right = None
+        self.display.setFocus()
 
         if result != 'error':
             self._left = result
     
+    @Slot()
+    def _backspace(self):
+        self.display.backspace()
+        self.display.setFocus()
+        
     def _makeDialog(self, text):
         msgBox = self.window.makeMsgBox()
         msgBox.setText(text)
--- python_curso_completo/m07_ui_pyside6/aula347_calculadora/utils.py
+++ python_curso_completo/m07_ui_pyside6/aula347_calculadora/utils.py
@@ -5,6 +5,17 @@ NUM_OR_DOT_REGEX = re.compile(r'^[0-9.]$')
 def isNumOrDot(string: str):
     return bool(NUM_OR_DOT_REGEX.search(string))
 
+def convertToNumber(string: str):      
+    if not isValidNumber(string):
+        return
+    
+    number = float(string)
+  
+    if number.is_integer():
+        number = int(number)
+
+    return number
+
 def isValidNumber(string: str):
     valid = False
     try:
